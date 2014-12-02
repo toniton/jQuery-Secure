0546abdda3637ff0330b62c3b8790900ace625f2
Akinjiola Toni <akinjiolatoni@gmail.com>
2014-12-02 16:38:01 +0100

    changed some css and removed unnecessary divs

--- example/js/usage.js
+++ example/js/usage.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	$('#dove').secure({count:9,scramble:true,theme:"purple",width:200,height:30});
+	$('.dove').secure({count:9,scramble:true,theme:"purple",width:200,height:30});
 });
